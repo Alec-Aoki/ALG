a57ec0d31d5296bb686094d878931fac976b8507
Alec Aoki <alec.camposaoki@gmail.com>
2024-08-19 17:09:01 -0300

    Add pilha

--- Aula03/pilha.c
+++ Aula03/pilha.c
@@ -3,4 +3,69 @@
 #include <stdbool.h>
 
 #include "item.h"
+#include "pilha.h"
 
+#define TAM 100
+
+struct pilha_{
+    ITEM *item[TAM]; //vetor de ponteiros para itens
+    int topo; //guarda o índice do topo da pilha
+};
+
+PILHA *pilha_criar(void){
+    PILHA *pilhaTemporaria = (PILHA *)malloc(sizeof(PILHA));
+    if(pilhaTemporaria != NULL){
+        pilhaTemporaria->topo = 0;
+    }
+
+    return pilhaTemporaria;
+}
+
+void pilha_apagar(PILHA **pilha){
+    if(*pilha != NULL){
+        free(*pilha);
+        *pilha = NULL;
+    }
+    return;
+}
+
+bool pilha_empilhar(PILHA *pilha, ITEM *item){
+    if((pilha == NULL) || (pilha_cheia(pilha)) || (item == NULL)) return false;
+
+    pilha->item[pilha->topo] = item;
+    pilha->topo += 1;
+
+    return true;
+}
+
+ITEM *pilha_desempilhar(PILHA *pilha){
+    if(pilha_vazia(pilha)) return NULL;
+
+    ITEM *itemTemporario = pilha->item[pilha->topo-1];
+    pilha->item[pilha->topo-1] = NULL;
+    pilha->topo -= 1;
+
+    return itemTemporario;
+}
+
+ITEM *pilha_topo(PILHA *pilha){
+    if(pilha == NULL) return NULL;
+
+    return pilha->item[pilha->topo-1];
+}
+
+int pilha_tamanho(PILHA *pilha){
+    if(pilha == NULL) return -1;
+
+    return pilha->topo;
+}
+
+bool pilha_vazia(PILHA *pilha){
+    if(pilha == NULL) return true;
+}
+
+bool pilha_cheia(PILHA *pilha){
+    if(pilha == NULL) return false;
+
+    if(pilha->topo == TAM+1) return true;
+}
