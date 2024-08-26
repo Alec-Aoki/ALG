7c14dba8fa3621d0cc8c16f1b7aea223f993cd06
Alec Aoki <alec.camposaoki@gmail.com>
2024-08-26 13:52:55 -0300

    Finish main

--- Aula04/notacaoPolonesa/main.c
+++ Aula04/notacaoPolonesa/main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include "Pilha.h"
 #include "item.h"
 
 int main(void){
+    char string[101];
+
+    scanf(" %s", string);
+
+    printf("%f\n", rpn(string));
 
     return 0;
 }
