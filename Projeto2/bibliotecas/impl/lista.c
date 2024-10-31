4d72626f0332ca3efcb0b89182cb684f3dc13388
Alec Aoki <alec.camposaoki@gmail.com>
2024-10-31 16:25:21 -0300

    Fix lista

--- Projeto2/bibliotecas/impl/lista.c
+++ Projeto2/bibliotecas/impl/lista.c
@@ -2,73 +2,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+
 #include "lista.h"
 
-typedef struct Lista_ {
-    int *dados;
+struct lista_ {
+    int vet[TAM_MAX];
     int tamanho;
-    int capacidade;
-}LISTA;
+};
 
 // Função auxiliar para busca binária
 bool busca_binaria(LISTA *lista, int elemento) {
+    if(lista == NULL) return false;
+    if(lista->tamanho == 0) return false;
+
     int esquerda = 0, direita = lista->tamanho - 1;
+
     while (esquerda <= direita) {
         int meio = esquerda + (direita - esquerda) / 2;
-        if (lista->dados[meio] == elemento) {
+        if (lista->vet[meio] == elemento) {
             return true;
-        } else if (lista->dados[meio] < elemento) {
+        } else if (lista->vet[meio] < elemento) {
             esquerda = meio + 1;
         } else {
             direita = meio - 1;
         }
     }
+
     return false;
 }
 
-LISTA* Lista_Criar(int capacidade) {
+LISTA* Lista_Criar(void) {
     LISTA *lista = (LISTA*)malloc(sizeof(LISTA));
-    lista->dados = (int*)malloc(capacidade * sizeof(int));
-    lista->tamanho = 0;
-    lista->capacidade = capacidade;
+    if(lista != NULL){
+        lista->tamanho = 0;
+    }
+
     return lista;
 }
 
-void Lista_Apagar(LISTA *lista) {
-    free(lista->dados);
-    free(lista);
+void Lista_Apagar(LISTA **lista) {
+    if(*lista == NULL) return;
+
+    free(*lista);
+    *lista = NULL;
+    return;
 }
 
 void Lista_Inserir(LISTA *lista, int elemento) {
-    if (lista->tamanho >= lista->capacidade) {
-        lista->capacidade *= 2;
-        lista->dados = (int*)realloc(lista->dados, lista->capacidade * sizeof(int));
-    }
+    if(lista == NULL) return;
+    if(lista->tamanho == TAM_MAX) return;
 
     // Encontra posição para inserção para manter a ordenação
     int i = lista->tamanho - 1;
-    while (i >= 0 && lista->dados[i] > elemento) {
-        lista->dados[i + 1] = lista->dados[i];
+    while (i >= 0 && lista->vet[i] > elemento) {
+        lista->vet[i + 1] = lista->vet[i];
         i--;
     }
-    lista->dados[i + 1] = elemento;
+    lista->vet[i + 1] = elemento;
     lista->tamanho++;
+
+    return;
 }
 
-void Lista_Remover(LISTA *lista) {
-    if (lista->tamanho > 0) {
-        lista->tamanho--;
-    }
+int Lista_Remover(LISTA *lista) {
+    if(lista == NULL) return ERRO;
+    if(lista->tamanho == 0) return ERRO;
+
+    int temp = lista->vet[lista->tamanho];
+    lista->vet[lista->tamanho] = ERRO;
+    lista->tamanho--;
+
+    return ERRO;
 }
 
 void Lista_Imprimir(LISTA *lista) {
+    if(lista == NULL) return;
+    if(lista->tamanho == 0) return;
+
     for (int i = 0; i < lista->tamanho; i++) {
-        printf("%d ", lista->dados[i]);
+        printf("%d ", lista->vet[i]);
     }
     printf("\n");
 }
 
 int Lista_Consultar (LISTA *lista, int i){
-    return lista->dados[i];
+    if(lista == NULL) return ERRO;
+    if(i > lista->tamanho) return ERRO;
+
+    return lista->vet[i];
 } 
 
--- Projeto2/bibliotecas/lista.h
+++ Projeto2/bibliotecas/lista.h
@@ -2,12 +2,15 @@
     #define LISTA_H
     #include <stdbool.h>
 
-    typedef struct Lista_ LISTA;
+    #define TAM_MAX 100000
+    #define ERRO 1000010
 
-    void Lista_Apagar (LISTA *lista); 
-    void Lista_Remover (LISTA *lista);
+    typedef struct lista_ LISTA;
+
+    void Lista_Apagar (LISTA **lista); 
+    int Lista_Remover (LISTA *lista);
     void Lista_Imprimir (LISTA *lista);
-    LISTA* Lista_Criar (int tamanho_dados);
+    LISTA* Lista_Criar (void);
     int Lista_Consultar (LISTA *lista, int indice);
     bool busca_binaria(LISTA *lista, int elemento);  
     void Lista_Inserir (LISTA* lista, int elemento);
