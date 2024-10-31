077ce708ef2d69192d26751540841b32995aa05e
Alec Aoki <alec.camposaoki@gmail.com>
2024-10-31 16:45:42 -0300

    Add conjunto_criar

---
+++ Projeto2/bibliotecas/impl/conjunto.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "../lista.h"
+#include "../heapmax.h"
+#include "../conjunto.h"
+
+struct conjunto_{
+  int TAD;
+  LISTA *conjuntoLista;
+  HEAPMAX *conjuntoHeap;
+  int tamanho;
+};
+
+CONJUNTO *conjunto_criar(int TAD){
+  if((TAD != TAD_LISTA) || (TAD != TAD_ARVORE)){
+    printf("TAD inválido\n");
+    return NULL;
+  }
+  
+  CONJUNTO *conjunto = (CONJUNTO *) malloc(sizeof(CONJUNTO));
+  if(conjunto == NULL){
+    printf("Erro em conjunto_criar: conjunto == NULL\n");
+    return NULL;
+  }
+
+  conjunto->TAD = TAD;
+  conjunto->tamanho = 0;
+  if(TAD == TAD_LISTA){
+    conjunto->conjuntoLista = Lista_Criar();
+    conjunto->conjuntoHeap = NULL;
+  }
+  else if(TAD == TAD_ARVORE){
+    conjunto->conjuntoHeap = heapmax_criar();
+    conjunto->conjuntoLista = NULL;
+  }
+
+  return conjunto;
+}
